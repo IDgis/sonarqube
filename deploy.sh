780115477c81150616ab8addde91feee35c33f42
RobStek <rob.stekelenburg@idgis.nl>
2017-01-02 11:51:29 +0100

    move versions to yaml

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 
 echo deploying...
 
-export SQ_VERSION=5.6
-
 export SQ_JDBC_USER=sonar
 export SQ_JDBC_PASSW=secret
 export SQ_PORT_INTERN=9000
@@ -12,8 +10,6 @@ export SQ_PORT_EXTERN=9009
 COMPOSE_ARGS="\
         -f docker-compose.yml \
         -p loc"
-
-docker pull sonarqube:$SQ_VERSION
 
 docker-compose \
         $COMPOSE_ARGS \
