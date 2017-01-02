3e3e449f930ededf78890dfccf329212485943b6
RobStek <rob.stekelenburg@idgis.nl>
2017-01-02 11:10:44 +0100

    add sonarqube portmapping

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ export SQ_VERSION=5.6
 
 export SQ_JDBC_USER=sonar
 export SQ_JDBC_PASSW=secret
+export SQ_PORT_INTERN=9000
+export SQ_PORT_EXTERN=9009
 
 COMPOSE_ARGS="\
         -f docker-compose.yml \
