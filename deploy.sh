f8a62e7c35bc253e28b94e5c088aaadd04bcc94b
RobStek <rob.stekelenburg@idgis.nl>
2017-01-02 10:20:34 +0100

    simplified: removed links, port-mappings

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,10 @@
 echo deploying...
 
 export PG_VERSION=9.5
-export PG_PORT_INTERN=5432
-export PG_PORT_EXTERN=5432
 
 export SQ_VERSION=5.6
-export SQ_PG_HOST=pgsonar
-export SQ_PORT_INTERN=9000
-export SQ_PORT_EXTERN=9000
 export SQ_JDBC_USER=sonar
 export SQ_JDBC_PASSW=secret
-export SQ_JDBC_URL=jdbc:postgresql://$SQ_PG_HOST:$PG_PORT_EXTERN/sonar
 
 COMPOSE_ARGS="\
         -f docker-compose.yml \
